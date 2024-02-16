9cbd265eed5ffddf9d45d72e13cfa9851594d679
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-16 16:33:23 -0600

    feat: added license

---
+++ build/assets/Cart-DVQn-q3m.js
@@ -0,0 +1,3 @@
+import{u as r,j as t}from"./index-C4yAYeyx.js";const a=r.div`
+  display: block;
+`;function o(){return t.jsx(a,{children:"Cart"})}export{o as default};
---
+++ build/assets/Privacy-DkjzNRFe.js
@@ -0,0 +1,3 @@
+import{u as r,j as a}from"./index-C4yAYeyx.js";const i=r.div`
+  display: block;
+`;function s(){return a.jsx(i,{children:"Privacy"})}export{s as default};
