883bc432bbaf17c743a0a427fc3b459d6b2edbfe
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-17 11:22:53 -0600

    Updates

--- assets/Privacy-BwqtnDpX.js
+++ assets/Privacy-BwqtnDpX.js
@@ -1,3 +1,3 @@
-import{u as r,j as a}from"./index-C4yAYeyx.js";const i=r.div`
+import{u as r,j as a}from"./index-C7_j3Zk_.js";const i=r.div`
   display: block;
 `;function s(){return a.jsx(i,{children:"Privacy"})}export{s as default};
