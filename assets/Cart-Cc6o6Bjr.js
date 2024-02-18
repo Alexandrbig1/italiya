68e71d0e6788b171b644d94961a4db77963a5469
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-17 19:17:24 -0600

    Updates

--- assets/Cart-Cc6o6Bjr.js
+++ assets/Cart-Cc6o6Bjr.js
@@ -1,3 +1,3 @@
-import{u as r,j as t}from"./index-C7_j3Zk_.js";const a=r.div`
+import{u as r,j as t}from"./index-Mphq3aJP.js";const a=r.div`
   display: block;
 `;function o(){return t.jsx(a,{children:"Cart"})}export{o as default};
---
+++ assets/SignUp-D38ph3k0.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-Mphq3aJP.js";function r(){return i.jsx("div",{children:"SignUp"})}export{r as default};
