347d5389f4e7d68dbccf8cc29d45d2c565f59792
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-22 12:22:58 -0600

    Updates

--- assets/Privacy-BSj3BSft.js
+++ assets/Privacy-BSj3BSft.js
@@ -1,3 +1,3 @@
-import{u as r,j as a}from"./index-Dry7BwFM.js";const i=r.div`
+import{u as r,j as a}from"./index-C8YgIz7n.js";const i=r.div`
   display: block;
 `;function s(){return a.jsx(i,{children:"Privacy"})}export{s as default};
--- assets/Terms--GzMt84R.js
+++ assets/Terms--GzMt84R.js
@@ -1,3 +1,3 @@
-import{u as r,j as s}from"./index-Dry7BwFM.js";const e=r.div`
+import{u as r,j as s}from"./index-C8YgIz7n.js";const e=r.div`
   display: block;
 `;function o(){return s.jsx(e,{children:"Therms"})}export{o as default};
